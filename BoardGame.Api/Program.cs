5dc117d2adecd8c1d32b784cb4b0927c79f6bcc4
andressaaihr <135823951+andressaaihr@users.noreply.github.com>
2024-06-04 19:28:21 +0200

    renamed namespace

--- BoardGame.Api/Program.cs
+++ BoardGame.Api/Program.cs
@@ -1,16 +1,17 @@
-using EFExemplo;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using boardGame;
 using boardGame.Cartas;
+using boardGame.Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddTransient<ICrud, CartasService>();
+// builder.Services.AddTransient<Context, Context>();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<Context>(options => 
+builder.Services.AddDbContext<ContextLorcana>(options => 
     options.UseNpgsql("Server=localhost;Port=5432;Database=sundaydatabase;UserId=postgres;Password=postgres;Include Error Detail=true;"));
 builder.Services.AddControllers();
 
